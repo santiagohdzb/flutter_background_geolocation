ec78361122038357b826a4a79c7ffef963771beb
Santiago Hernández <santiago.hdz.b@gmail.com>
2020-01-14 15:19:08 -0600

    mas cambios para android

--- example/android/app/src/main/java/com/transistorsoft/flutterbackgroundgeolocationexample/Application.java
+++ example/android/app/src/main/java/com/transistorsoft/flutterbackgroundgeolocationexample/Application.java
@@ -1,42 +1,6 @@
 package com.transistorsoft.flutterbackgroundgeolocationexample;
 
-import android.os.StrictMode;
-
-import com.transistorsoft.flutter.backgroundfetch.BackgroundFetchPlugin;
-import com.transistorsoft.flutter.backgroundgeolocation.FLTBackgroundGeolocationPlugin;
-
 import io.flutter.app.FlutterApplication;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class Application extends FlutterApplication {
-//    @Override
-//    public void onCreate() {
-//        // Strict mode.  Should be disabled on RELEASE.
-//        /*
-//        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-//                .detectDiskReads()
-//                .detectDiskWrites()
-//                .detectAll()
-//                .penaltyLog()
-//                .build());
-//
-//        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-//                .detectLeakedSqlLiteObjects()
-//                .detectLeakedClosableObjects()
-//                .penaltyLog()
-//                .penaltyDeath()
-//                .build());
-//         */
-//
-//        super.onCreate();
-//
-//        FLTBackgroundGeolocationPlugin.setPluginRegistrant(this);
-//        BackgroundFetchPlugin.setPluginRegistrant(this);
-//    }
-//
-//    @Override
-//    public void registerWith(PluginRegistry registry) {
-//        GeneratedPluginRegistrant.registerWith(registry);
-//    }
 }
