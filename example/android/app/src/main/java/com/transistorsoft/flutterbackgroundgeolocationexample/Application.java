7ad32c198c051d28c86da14885afb2a7d6b41141
Santiago Hernández <santiago.hdz.b@gmail.com>
2020-01-14 12:49:47 -0600

    removed more android things

--- example/android/app/src/main/java/com/transistorsoft/flutterbackgroundgeolocationexample/Application.java
+++ example/android/app/src/main/java/com/transistorsoft/flutterbackgroundgeolocationexample/Application.java
@@ -9,34 +9,34 @@ import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class Application  extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
-    @Override
-    public void onCreate() {
-        // Strict mode.  Should be disabled on RELEASE.
-        /*
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads()
-                .detectDiskWrites()
-                .detectAll()
-                .penaltyLog()
-                .build());
-
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                .detectLeakedSqlLiteObjects()
-                .detectLeakedClosableObjects()
-                .penaltyLog()
-                .penaltyDeath()
-                .build());
-         */
-
-        super.onCreate();
-
-        FLTBackgroundGeolocationPlugin.setPluginRegistrant(this);
-        BackgroundFetchPlugin.setPluginRegistrant(this);
-    }
-
-    @Override
-    public void registerWith(PluginRegistry registry) {
-        GeneratedPluginRegistrant.registerWith(registry);
-    }
+public class Application extends FlutterApplication {
+//    @Override
+//    public void onCreate() {
+//        // Strict mode.  Should be disabled on RELEASE.
+//        /*
+//        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//                .detectDiskReads()
+//                .detectDiskWrites()
+//                .detectAll()
+//                .penaltyLog()
+//                .build());
+//
+//        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+//                .detectLeakedSqlLiteObjects()
+//                .detectLeakedClosableObjects()
+//                .penaltyLog()
+//                .penaltyDeath()
+//                .build());
+//         */
+//
+//        super.onCreate();
+//
+//        FLTBackgroundGeolocationPlugin.setPluginRegistrant(this);
+//        BackgroundFetchPlugin.setPluginRegistrant(this);
+//    }
+//
+//    @Override
+//    public void registerWith(PluginRegistry registry) {
+//        GeneratedPluginRegistrant.registerWith(registry);
+//    }
 }
