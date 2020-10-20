72bcd75306f1e6100ddb09d3dc3e8eb04a99ddaf
Santiago Hernandez <santiago.hdz.b@gmail.com>
2020-10-20 09:14:15 -0500

    done

--- example/android/app/src/main/java/com/transistorsoft/flutterbackgroundgeolocationexample/Application.java
+++ example/android/app/src/main/java/com/transistorsoft/flutterbackgroundgeolocationexample/Application.java
@@ -2,12 +2,6 @@ package com.transistorsoft.flutterbackgroundgeolocationexample;
 
 import android.content.Context;
 import android.util.Log;
-
-import com.transistorsoft.locationmanager.adapter.BackgroundGeolocation;
-import com.transistorsoft.locationmanager.adapter.callback.TSBeforeInsertBlock;
-import com.transistorsoft.locationmanager.location.TSLocation;
-import com.transistorsoft.locationmanager.logger.TSLog;
-
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -18,33 +12,6 @@ import io.flutter.app.FlutterApplication;
 public class Application  extends FlutterApplication {
     @Override
     public void onCreate() {
-        ////
-        // Strict mode.  Should be disabled on RELEASE.
-        // NOTE:  This is NOT required for background_geolocation
-        //
-        /*
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads()
-                .detectDiskWrites()
-                .detectAll()
-                .penaltyLog()
-                .build());
-
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                .detectLeakedSqlLiteObjects()
-                .detectLeakedClosableObjects()
-                .penaltyLog()
-                .penaltyDeath()
-                .build());
-
-        */
-
         super.onCreate();
-
     }
-
-
-
-
 }
-
